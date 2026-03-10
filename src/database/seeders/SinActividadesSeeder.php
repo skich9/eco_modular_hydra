7e288dc4bc11a72cf5c47fc6c000e57a08c9af10
Herland <herland.veliz.127@gmail.com>
2026-03-10 10:12:48 -0400

    correcion en seeeder de sin actividades en timestamps que no correspondian

--- src/database/seeders/SinActividadesSeeder.php
+++ src/database/seeders/SinActividadesSeeder.php
@@ -14,22 +14,16 @@ class SinActividadesSeeder extends Seeder
 				'codigo_caeb' => '853000',
 				'descripcion' => 'ENSEÑANZA SUPERIOR',
 				'tipo_actividad' => 'P',
-				'created_at' => now(),
-				'updated_at' => now(),
 			],
 			[
 				'codigo_caeb' => '8530400',
 				'descripcion' => 'Docencia en Educación Técnica y No Universitaria',
 				'tipo_actividad' => 'P',
-				'created_at' => now(),
-				'updated_at' => now(),
 			],
 			[
 				'codigo_caeb' => '854000',
 				'descripcion' => 'ENSEÑANZA DE ADULTOS Y OTROS TIPOS DE ENSEÑANZA',
 				'tipo_actividad' => 'S',
-				'created_at' => now(),
-				'updated_at' => now(),
 			],
 		]);
 	}
